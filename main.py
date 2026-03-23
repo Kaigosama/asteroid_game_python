51b5392a123ffff1ce4ce86e83f61cab4f6bbd00
Kaigosama <guiangk54@gmail.com>
2026-03-24 01:40:23 +0800

    moving triangle player

--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
     print("Starting Asteroids with pygame version: 2.6.1")
     print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    
+    clock_variable = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         log_state()
         for event in pygame.event.get():
-            pass
-        screen.fill("black")
-        pygame.display.flip() 
-        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
+        screen.fill("black")
+        for draws in drawable:
+            draws.draw(screen)
+        pygame.display.flip() 
+
+        dt = clock_variable.tick(60)/1000
 
 
 if __name__ == "__main__":
