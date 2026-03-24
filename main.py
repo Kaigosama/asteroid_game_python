99dc06bf9a17ebc201d4243f676f603ef8135ae1
Kaigosama <guiangk54@gmail.com>
2026-03-24 23:09:52 +0800

    added asteroid destroyed feature

--- main.py
+++ main.py
@@ -40,6 +40,12 @@ def main():
                 log_event("player_hit")
                 return print("Game over!")
                 sys.exit()
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    asteroid.kill()
+                    shot.kill()
         screen.fill("black")
         for draws in drawable:
             draws.draw(screen)
